a5800a161c841255d0b905bc9f388b68bbf03ea4
arsanynagy2023-wq <arsanynagy@gmail.com>
2026-07-05 00:34:59 +0300

    fix mobile header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,34 +3,53 @@ import "../styles/header.css";
 
 export default function Header() {
   return (
-    <header className="header-wrapper">
+    <nav className="navbar">
 
-      <nav className="navbar">
+      <div className="logo">
+        <span>√x</span>
 
-        <div className="logo-box">
-          <div className="math-logo">√x</div>
-
-          <div>
-            <h2>Mr. Kareem</h2>
-            <span>Math Teacher</span>
-          </div>
+        <div className="logo-text">
+          <h2>Mr. Kareem</h2>
+          <p>Math Teacher</p>
         </div>
+      </div>
 
-        <ul className="nav-links">
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/courses">Courses</Link></li>
-          <li><Link to="/about">About</Link></li>
-          <li><Link to="/contact">Contact</Link></li>
-          <li><Link to="/register">Register</Link></li>
-        </ul>
+      <ul className="nav-links">
 
-        <div className="lang-buttons">
-          <button>AR</button>
-          <button>EN</button>
-        </div>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
 
-      </nav>
+        <li>
+          <Link to="/courses">Courses</Link>
+        </li>
 
-    </header>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+
+        <li>
+          <Link to="/register">Register</Link>
+        </li>
+
+      </ul>
+
+      <div className="language-switch">
+
+        <button className="active-lang">
+          AR
+        </button>
+
+        <button>
+          EN
+        </button>
+
+      </div>
+
+    </nav>
   );
 }
